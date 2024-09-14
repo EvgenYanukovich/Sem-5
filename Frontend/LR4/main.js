5a7bcae5f62ef7f1e5edb70f630b012ea375da89
Evgeniy <evgen.yanukovich@gmail.comm>
2024-09-14 10:07:01 +0300

    Labs made before 14.09.2024

---
+++ Frontend/LR4/main.js
@@ -0,0 +1,40 @@
+var array = [
+    { id: 1, name: 'Vasya', group: 10 },
+    { id: 2, name: 'Ivan', group: 11 },
+    { id: 3, name: 'Masha', group: 12 },
+    { id: 4, name: 'Petya', group: 10 },
+    { id: 5, name: 'Kira', group: 11 }
+];
+var car = {
+    manufacturer: "manufacturer",
+    model: "model"
+};
+var car1 = {
+    manufacturer: "manufacturer",
+    model: "model"
+};
+var car2 = {
+    manufacturer: "manufacturer",
+    model: "model"
+};
+var arrayCars = [
+    {
+        cars: [car1, car2]
+    }
+];
+var group = {
+    students: [],
+    studentsFilter: function (groupNumber) {
+        return this.students.filter(function (student) { return student.group === groupNumber; });
+    },
+    marksFilter: function (markNumber) {
+        return this.students.filter(function (student) {
+            return student.marks.some(function (mark) { return mark.mark === markNumber; });
+        });
+    },
+    deleteStudent: function (id) {
+        this.students = this.students.filter(function (student) { return student.id !== id; });
+    },
+    mark: 10,
+    group: 12
+};
