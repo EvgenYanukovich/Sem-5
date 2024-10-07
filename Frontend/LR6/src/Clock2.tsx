9594c44397e29affa348b8a1db64e2de9f328a9f
Evgeniy <evgen.yanukovich@gmail.comm>
2024-10-07 08:26:37 +0300

    Labs made before 07.10.2024

--- Frontend/LR6/src/Clock2.tsx
+++ Frontend/LR6/src/Clock2.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Clock.css';
 
-const Clock2: React.FC = () => {
+const Clock: React.FC = () => {
   const [time, setTime] = useState<Date>(new Date());
 
   useEffect(() => {
@@ -17,22 +17,50 @@ const Clock2: React.FC = () => {
   const hours = time.getHours();
 
   const secondDegrees = (seconds / 60) * 360;
-  const minuteDegrees = (minutes / 60) * 360 + (seconds / 60) * 6; 
-  const hourDegrees = ((hours % 12) / 12) * 360 + (minutes / 60) * 30; 
+  const minuteDegrees = (minutes / 60) * 360 + (seconds / 60) * 6;
+  const hourDegrees = ((hours % 12) / 12) * 360 + (minutes / 60) * 30;
+
+  // Генерация массива из 60 черточек
+  const ticks = Array.from({ length: 60 }, (_, i) => i);
 
   return (
     <div className="clock">
       <div className="clock-face">
+        {/* Генерация черточек */}
+        <div className="ticks-container">
+          {ticks.map((tick, i) => (
+            <div
+              key={i}
+              className={`tick`} // Каждая 5-я черточка длиннее
+              style={{ transform: `rotate(${i * 6}deg) translateY(-120px)` }} // Поворот и смещение
+            />
+          ))}
+        </div>
+        <div className="ticks-container">
+          {ticks.map((tick2, i) => (
+            <div
+              key={i}
+              className={`tick2`} // Каждая 5-я черточка длиннее
+              style={{ transform: `rotate(${i * 30}deg) translateY(-120px)` }} // Поворот и смещение
+            />
+          ))}
+        </div>
+
+        {/* Часовая стрелка */}
         <div
           className="hand hour-hand"
           style={{ transform: `rotate(${hourDegrees}deg)` }}
         ></div>
+
+        {/* Минутная стрелка */}
         <div
           className="hand minute-hand"
           style={{ transform: `rotate(${minuteDegrees}deg)` }}
         ></div>
+
+        {/* Секундная стрелка */}
         <div
-          className="hand second-hand"
+          className={`hand second-hand ${seconds === 0 ? 'no-transition' : ''}`}
           style={{ transform: `rotate(${secondDegrees}deg)` }}
         ></div>
       </div>
@@ -40,4 +68,4 @@ const Clock2: React.FC = () => {
   );
 };
 
-export default Clock2;
+export default Clock;
